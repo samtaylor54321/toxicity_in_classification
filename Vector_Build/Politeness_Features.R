0d52f7fbbc9b530ebc24fdbf978158a8f00fe9e4
Sam Taylor <shane-warnie-warne@hotmail.co.uk>
2019-04-10 15:29:39 +0100

    added updated politeness features script

--- Vector_Build/Politeness_Features.R
+++ Vector_Build/Politeness_Features.R
@@ -1,5 +1,5 @@
 
-######################################  BUILD POLITENESS FEATURES ################################
+######################################  BUILD POLITENESS FEATURES ###########################################
  
 # Function which uses the politness package to build a number of features relating to the politeness of 
 # particular comments
@@ -7,7 +7,8 @@
 # Load Packages -----------------------------------------------------------
 
 library(tidyverse)
-library(politness)
+library(politeness)
+library(spacyr)
 
 # Load Datasets ----------------------------------------------------------
 
@@ -20,21 +21,21 @@ test <- read_csv("/Users/samtaylor/Downloads/jigsaw-unintended-bias-in-toxicity-
 
 # Build Politeness Features -----------------------------------------------
 
+# initialise spacy wrapper to make sure that all possible features are created for modelling. NB this should 
+# already be working correctly
+
+spacyr::spacy_initialize()
+
+# sets test to understand how long it takes to create this matrix 
+
+start_time <- Sys.time()
+
+polite_features <- politeness(train$comment_text, metric ='count', drop_blank =TRUE, parser='spacy')
+
+end_time <- Sys.time()
+
+# need to think of a way to output and getting this working in python - building a joint dataset?
+# set up a script which takes in all CSV files and outputs a final dataset which can be read in by either R or Python. 
+# RDS?
 
 
-
-
-
-
-
-
-# building polite features 
-
-
-
-
-
-
-
-install.packages('politeness')
-library(politeness)
