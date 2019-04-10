4ab52e93804e2f752e6a853d9462f8d1eaac2db5
James Shepherd <jamesshepherd413@jamess-mbp.home>
2019-04-10 20:53:41 +0100

    Added main model running script

--- Model_Build/base.py
+++ Model_Build/base.py
@@ -1,10 +1,12 @@
 import os
+import gc
 import pickle
 import tensorflow as tf
 import keras.backend as K
 from datetime import datetime
 from keras.layers import embeddings
 from keras.callbacks import EarlyStopping
+from sklearn.model_selection import StratifiedKFold
 from .utils import *
 
 class BaseClassifier:
@@ -13,12 +15,18 @@ class BaseClassifier:
     """
 
     def __init__(self, params):
+        """ Set generic parameters """
+        self.run_config = params
         self.embedding_path = params['embedding_path']
         self.identity_data_path = params['identity_data_path']
+        self.batch_size = params['batch_size']
+        self.epochs = params['epochs']
         self.bias_identities = get_identities()
         self.run_timestamp = datetime.now().strftime('%Y%m%d_%H.%M.%S')
         self.comp_metric = None
+        self.cv_comp_metrics = []
         self.result = {}
+        self.cv_results = []
 
     def embedding_as_keras_layer(self):
         """ Load specified embedding as a Keras layer """
@@ -26,21 +34,21 @@ class BaseClassifier:
         if isinstance(embedding, embeddings.Embedding):
             return embedding
         else:
-            raise TypeError('Embedding at {} can\'t be converted to keras layer'.
-                            format(self.embedding_path))
+            raise TypeError('Embedding at {} can\'t be converted to keras layer'
+                            .format(self.embedding_path))
 
     def create_model(self):
         pass
 
-    def train(self, X_train, y_train, batch_size, max_epochs, validation_data=None):
+    def train(self, X_train, y_train, validation_data=None):
         """ Define a training function with early stopping """
         self.model = self.create_model()
         self.result = self.model.fit(
             X_train,
             y_train,
             validation_data=validation_data,
-            batch_size=batch_size,
-            epochs=max_epochs,
+            batch_size=self.batch_size,
+            epochs=self.epochs,
             callbacks=[
                 EarlyStopping(
                     monitor='val_loss',
@@ -54,12 +62,39 @@ class BaseClassifier:
         if validation_data:
             train = pd.read_csv(self.identity_data_path)
             train[self.bias_identities].fillna(0, inplace=True)
-            train.loc[:, self.bias_identities] = train.loc[:, self.bias_identities].astype(bool)
+            train.loc[:, self.bias_identities] = \
+                train.loc[:, self.bias_identities].astype(bool)
 
             y_pred = self.model.predict(validation_data[0])
-            bias_metrics_df = compute_bias_metrics_for_model(train, self.bias_identities,
-                                                             validation_data[1], y_pred)
-            self.comp_metric = get_final_metric(bias_metrics_df, roc_auc_score(validation_data[1], y_pred))
+            bias_metrics_df = compute_bias_metrics_for_model(
+                train, self.bias_identities, validation_data[1], y_pred
+            )
+            self.comp_metric = get_final_metric(
+                bias_metrics_df, roc_auc_score(validation_data[1], y_pred)
+            )
+
+    def cv(self, X, y, cv=StratifiedKFold(3)):
+        """ Apply training function in CV fold """
+        for fold_no, (train_idx, val_idx) in enumerate(cv.split(X, y)):
+            X_train = X[train_idx]
+            X_val = X[val_idx]
+            if isinstance(y, 'pd.Series'):
+                y_train = y.iloc[train_idx]
+                y_val = y.iloc[val_idx]
+            else:
+                y_train = y[train_idx]
+                y_val = y[val_idx]
+
+            self.train(X_train, y_train, [X_val, y_val])
+            self.cv_comp_metrics.append(self.comp_metric)
+            self.cv_results.append(self.result)
+
+        self.run_config['cv_comp_metrics'] = self.cv_comp_metrics
+        self.run_config['cv_results'] = self.cv_results
+        if fold_no < cv.get_n_splits() - 1:
+            K.clear_session()
+            del self.model
+            gc.collect()
 
     def save(self):
         pass
--- Model_Build/lstm.py
+++ Model_Build/lstm.py
@@ -1,20 +1,22 @@
-import os
+import yaml
 import pickle
+from pathlib import Path
 from .base import BaseClassifier
 from keras.models import Sequential
 from keras.layers import Dense, LSTM
+
 
 class LSTMClassifier(BaseClassifier):
 
     def __init__(self, params):
         super().__init__(params)
-        self.lstm_nodes = params['lstm_nodes']
+        self.lstm_units = params['lstm_units']
 
     def create_model(self):
         model = Sequential()
         embedding = self.embedding_as_keras_layer()
         model.add(embedding)
-        model.add(LSTM(units=self.lstm_nodes,
+        model.add(LSTM(units=self.lstm_units,
                        dropout=0.2,
                        recurrent_dropout=0.2))
         model.add(Dense(units=1,
@@ -26,7 +28,15 @@ class LSTMClassifier(BaseClassifier):
         )
         return model
 
-    def save(self):
-        out_path = os.path.join('Trained_Models', 'lstm_' + self.run_timestamp + '.pkl')
-        pickle.dump(self.model, open(out_path, 'wb'))
+    def save(self, dir):
+        out_dir = Path(dir)
+        out_dir = out_dir / self.run_timestamp
+        out_dir.mkdir(parents=True, exist_ok=True)
 
+        model_out_path = out_dir / 'MODEL_lstm.pkl'
+        with open(model_out_path, 'wb') as model_out:
+            pickle.dump(self.model, model_out)
+
+        results_out_path = outdir / 'RESULTS_lstm.csv')
+        with open(results_out_path, 'w') as results_out:
+            yaml.dump(self.run_config, results_out)
