b1d3e329833fbb851e53c768c03b290592280994
Sam Taylor <shane-warnie-warne@hotmail.co.uk>
2019-04-16 22:00:09 +0100

    added list of external datasets to repo and built script to start testing the value of these for the model

--- Vector_Build/Punctuation_Feature.R
+++ Vector_Build/Punctuation_Feature.R
@@ -8,17 +8,13 @@
 
 library(tidyverse)
 
-# Load Data ---------------------------------------------------------------
-
-train <- read_csv("/Users/samtaylor/Documents/Git/kaggle_jigsaw/kaggle_jigsaw/Data/train.csv")
-test <- read_csv("/Users/samtaylor/Documents/Git/kaggle_jigsaw/kaggle_jigsaw/Data/test.csv")
-
 # Build Function ----------------------------------------------------------
 
 punctuation <- function (.data, id_col, text_col) {
   .data <- .data %>% 
     dplyr::select(!!ensym(id_col), !!ensym(text_col)) %>% 
-    dplyr::mutate(exclamation = str_detect(!!ensym(text_col),'!'),
+    dplyr::mutate(id = !!ensym(id_col),
+                  exclamation = str_detect(!!ensym(text_col),'!'),
                   question = str_detect(!!ensym(text_col),'\\?'),
                   exclamation_count = str_count(!!ensym(text_col),'!'),
                   question_count = str_count(!!ensym(text_col),'\\?'),
@@ -28,11 +24,3 @@ punctuation <- function (.data, id_col, text_col) {
                   amp_count = str_count(!!ensym(text_col), '&'))
   .data 
 }
-
-ggplot(punc, aes(x=exclamation_count, y=toxicity)) + geom_point() + geom_smooth(method='lm')
-
-
-
-punc <- punctuation(train, id, comment_text)
-punc$toxicity <- train$target
-
